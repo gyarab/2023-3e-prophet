6f3e8cbe376dd79d2a9ccb957e72cfce8ed796d7
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2024-01-06 19:18:45 +0100

    renamed bot_logic to data_fetcher, deleted neural network part.

---
+++ data_fetcher.py
@@ -0,0 +1,53 @@
+import ccxt
+
+APIKEY = '01566309D7D54F6B83CD7BD57090B485'
+SECRETKEY = 'FF1A020B10EC704972C034475F2BBA140F814F87B401F37B'
+
+exchange = ccxt.coinex({
+        'apiKey': APIKEY,
+        'secret': SECRETKEY,
+    })
+
+def get_btc_price():
+    symbol = 'BTC/USDT'
+    # Fetch ticker information for BTC/USDT pair
+    ticker = exchange.fetch_ticker(symbol)
+
+    # Extract and print the last price
+    last_price = ticker['last']
+    
+    return last_price
+
+def get_last_30_btc_price():
+    symbol = 'BTC/USDT'
+    
+    # Fetch historical OHLCV data with 1-minute timeframe
+    #OHLCV stands for: open, high, low, close, volume
+    ohlcv = exchange.fetch_ohlcv(symbol, '1m') # Use '1m' for 1-minute timeframe
+
+    # last 30 minutes as ?list?
+    last_30_prices = ohlcv[-30:]
+    
+    return last_30_prices
+#returns account balance as dictoniary
+def get_balance():
+    
+    # Fetch your account balance
+    balance = exchange.fetch_balance()
+
+    return balance
+
+def get_markets():
+    markets = exchange.load_markets()
+
+    return list(markets.keys())
+
+if __name__ == '__main__':
+    # print(get_btc_price())
+    #print("CoinEx Account Balance:")
+    #print(get_balance())
+    
+    for candle in get_last_30_btc_price():
+        timestamp, open_, high, low, close, volume = candle
+        print(f'Timestamp: {timestamp}, Close Price: {close}')
+     
