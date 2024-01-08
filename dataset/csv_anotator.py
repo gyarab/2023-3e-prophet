6ca711c602c04b57194a712f1964abafb2dbde05
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2024-01-08 17:40:27 +0100

    added fucntion for upcoming price anotation

--- dataset/csv_anotator.py
+++ dataset/csv_anotator.py
@@ -1,5 +1,5 @@
 import csv
-import tqdm
+from tqdm import tqdm
 import os
 
 #adds to every row (candle) an anotation of next row (candle) close value
@@ -8,13 +8,32 @@ def anotate_target_value(input_file, output_file):
     # Get the absolute path of the input CSV file
     input_file_path = os.path.join(os.path.dirname(__file__),'data', input_file)
     output_file_path = os.path.join(os.path.dirname(__file__),'data',  output_file)
-    
+
+    # Reading the input CSV file
+    with open(input_file_path, 'r') as infile:
+        reader = csv.reader(infile)
+        data = list(reader)
+
+    # Creating a new CSV file
+    with open(output_file_path, 'w', newline='') as outfile:
+        writer = csv.writer(outfile)
+        
+        # Writing rows to the new CSV file
+        for i in tqdm(range(len(data) - 1), desc="Processing rows"):
+            #loads curent row
+            current_row = data[i]
+            #gets upcoming close value
+            upcoming_close_value = float(data[i+1][4])  # 'close' value from upcoming set of 60
+            #append at the end oh the row upcoming close valye
+            current_row.append(upcoming_close_value) # add the 0 or 1 at the end of the file
+            # Writing the concatenated row to the CSV file
+            writer.writerow(current_row)
 
 #adds to every row (candle) an anotation of next row (candle) close value difference
 def anotate_target_difference():
     pass
 
-if __name__ == '__name__':
-    input_csv_file = 'BTCUSDT.csv'
-    output_csv_file = 'target_v_BTCUSDT.csv'
+if __name__ == '__main__':
+    input_csv_file = 'test_BTCUSDT.csv'
+    output_csv_file = 'test_target_v_BTCUSDT.csv'
     anotate_target_value(input_csv_file, output_csv_file)
