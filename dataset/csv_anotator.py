c1da30b1bdbdb2e38345055faa58e08577196258
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2024-01-08 17:24:39 +0100

    update on csv_anotator

--- dataset/csv_anotator.py
+++ dataset/csv_anotator.py
@@ -2,9 +2,19 @@ import csv
 import tqdm
 import os
 
+#adds to every row (candle) an anotation of next row (candle) close value
+def anotate_target_value(input_file, output_file):
 
-def anotate_target_value():
-    pass
+    # Get the absolute path of the input CSV file
+    input_file_path = os.path.join(os.path.dirname(__file__),'data', input_file)
+    output_file_path = os.path.join(os.path.dirname(__file__),'data',  output_file)
+    
 
+#adds to every row (candle) an anotation of next row (candle) close value difference
 def anotate_target_difference():
     pass
+
+if __name__ == '__name__':
+    input_csv_file = 'BTCUSDT.csv'
+    output_csv_file = 'target_v_BTCUSDT.csv'
+    anotate_target_value(input_csv_file, output_csv_file)
