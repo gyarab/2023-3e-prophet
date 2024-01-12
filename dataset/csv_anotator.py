a51bb7e9a4bc5976ee0de7a980db37638152896e
VitekKvitek <vitezslav.prochazka@student.gyarab.cz>
2024-01-12 19:19:43 +0100

    removed old functions

--- dataset/csv_anotator.py
+++ dataset/csv_anotator.py
@@ -7,21 +7,12 @@ import os # file paths
 import pandas as pd
 import ta
 
-
-
-# def fully_anotate(input_file, output_file):
-#     # <anotate technical factors>
-#     anotate_target_value(input_file, output_file)
-#     anotate_target_difference(input_file, output_file)
-
-
-# returns an absolute path of file that is contained in data folder
-# data folder has to be relative to this script
 def get_absolute_path(input_file):
     input_file_path = os.path.join(os.path.dirname(__file__),'data', input_file)
     return input_file_path
 
-# adds anotation off technical indicators
+
+# adds anotation off technical indicators and target values and their differences
 def fully_anotate(csv_file, output_file):
     
     input_file_path = get_absolute_path(csv_file)
@@ -56,64 +47,6 @@ def fully_anotate(csv_file, output_file):
     
     # Save the modified dataframe with technical indicators to a new CSV file
     df.to_csv(output_file_path, index=False)
-   
-# adds to every row (candle) an anotation of next row (candle) close value
-def anotate_target_value(input_file, output_file):
-
-    # Get the absolute path of the input CSV file
-    input_file_path = get_absolute_path(input_file)
-    output_file_path = get_absolute_path(output_file)
-
-    # Reading the input CSV file
-    with open(input_file_path, 'r') as infile:
-        reader = csv.reader(infile)
-        data = list(reader)
-
-    # Creating a new CSV file
-    with open(output_file_path, 'w', newline='') as outfile:
-        writer = csv.writer(outfile)
-        
-        # Writing rows to the new CSV file
-        for i in tqdm(range(len(data) - 1), desc="Processing rows"):
-            # loads curent row
-            current_row = data[i]
-            # gets upcoming close value
-            upcoming_close_value = float(data[i+1][4])  # 'close' value from upcoming set of 60
-            # append at the end oh the row upcoming close valye
-            current_row.append(upcoming_close_value) # add the 0 or 1 at the end of the file
-            # Writing the concatenated row to the CSV file
-            writer.writerow(current_row)
-
-# adds to every row (candle) an anotation of next row (candle) close value difference
-def anotate_target_difference(input_file, output_file):
-    
-    # Get the absolute path of the input CSV file
-    input_file_path = get_absolute_path(input_file)
-    output_file_path = get_absolute_path(output_file)
-
-    # Reading the input CSV file
-    with open(input_file_path, 'r') as infile:
-        reader = csv.reader(infile)
-        data = list(reader)
-
-    # Creating a new CSV file
-    with open(output_file_path, 'w', newline='') as outfile:
-        writer = csv.writer(outfile)
-        
-        # Writing rows to the new CSV file
-        for i in tqdm(range(len(data) - 1), desc="Processing rows"):
-            # loads curent row
-            current_row = data[i]
-            # gets upcoming close value
-            upcoming_close_value = float(data[i+1][4])  # 'close' value from upcoming set of 60
-            # substract the current close valye with the upcoming close value
-            close_diffrence = float(current_row[4]) - upcoming_close_value
-            # rounds close_difference to three decimal digits (most data will be rounded to just two)
-            rounded_close_difference = round (close_diffrence, 5)
-            #append at the end oh the row upcoming close valye
-            current_row.append(rounded_close_difference) # add the 0 or 1 at the end of the file
-            # Writing the concatenated row to the CSV file
-            writer.writerow(current_row)
     
 # this function will create a new csv file that will put 0-59 row into one row and will add 0 or 1 at the end based upcoming close value
 # the order example : 0 - 59 , 1 - 60 , 2 - 61 , 3 - 62 and so on
