5bef9f5807bc9e7dd1c0faab776c3f106a12a45d
Jiriik <Jiri.petrik@student.gyarab.cz>
2024-03-21 19:55:39 +0100

    added support for css from local

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-import data_fetcher
+#import data_fetcher
 
 app = Flask(__name__)
 
@@ -13,7 +13,7 @@ def index():
 @app.route('/increment_btc_value')
 def increment_btc_value():
     global btc_value
-    btc_value = data_fetcher.get_btc_price()
+    #btc_value = data_fetcher.get_btc_price()
     return jsonify({'btc_value': btc_value})
 
 #if this script is being run directly, run it in debbug mode( detailed errors + some other features)
