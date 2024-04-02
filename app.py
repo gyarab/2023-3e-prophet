79ecb50a7576611a8419a93725572349491be49b
Jiriik <Jiri.petrik@student.gyarab.cz>
2024-04-02 19:57:28 +0200

    added json save file test

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from binance_data_fetcher import get_current_btc_value, get_last_hour_values
+from save_data import save_trading_data, load_trading_data
 
 
 app = Flask(__name__)
@@ -20,6 +21,22 @@ def get_btc_value():
 def get_last_hour_values_endpoint():
     last_hour_values = get_last_hour_values()
     return {'last_hour_values': last_hour_values}
+
+@app.route('/save_trading_data', methods=['POST'])
+def save_trading_data_endpoint():
+    data = request.get_json()
+    time_spent_trading = data['time_spent_trading']
+    btc_value_invested = data['btc_value_invested']
+    filename = "trading_data.json"
+    save_trading_data(time_spent_trading, btc_value_invested, filename)
+    return jsonify({'message': 'Trading data saved successfully'})
+
+@app.route('/load_trading_data')
+def load_trading_data_endpoint():
+    filename = "trading_data.json"
+    trading_data = load_trading_data(filename)
+    return jsonify({'trading_data': trading_data})
+
     
 
 #if this script is being run directly, run it in debbug mode( detailed errors + some other features)
