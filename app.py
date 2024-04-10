57e49598f02597ca0de00310999aeaeb880e4718
Jiriik <Jiri.petrik@student.gyarab.cz>
2024-04-10 15:45:23 +0200

    added route for starting the bot trading (error without cuda)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 from binance_data_fetcher import get_current_btc_value, get_last_hour_values
 from json_data_handler import update_trading_data, load_trading_data, reset_saved_data
+#from live_trading_bot import start_trading, stop_trading
 
 app = Flask(__name__)
 
@@ -44,8 +45,17 @@ def update_trading_data_endpoint():
     value = data['value']
     
     update_trading_data(key, value)
-    
     return jsonify({'message': 'Trading data has been updated'}), 200
+
+# @app.route('/start_trading', methods=['POST'])
+# def start_trading_route():
+    # start_trading()
+    # return jsonify({'message': 'Trading started'})
+# 
+# @app.route('/stop_trading', methods=['POST'])
+# def stop_trading_route():
+    # stop_trading()
+    # return jsonify({'message': 'Trading stopped'})
 
 
 # if this script is being run directly, run it in debug mode (detailed errors + some other features)
--- static/logic.js
+++ static/logic.js
@@ -7,35 +7,34 @@ let startTime = Date.now();
 //displayTimePassed();
 
 
-(function SiteRefresh() 
-{
-    loadData();
+(function SiteRefresh() {
+  loadData();
 
-    fetch('/get_last_hour_values')
-        .then(response => response.json())
-        .then(data => {
-            btcVal = Object.values(data)[0];
+  fetch('/get_last_hour_values')
+    .then(response => response.json())
+    .then(data => {
+      btcVal = Object.values(data)[0];
 
-            // Current btc price
-            document.getElementById('btc_value').textContent = btcVal[btcVal.length - 1];
+      // Current btc price
+      document.getElementById('btc_value').textContent = btcVal[btcVal.length - 1];
 
-            // Last hour diff
-            const btcHourDiff = (100 - ((100 / btcVal[btcVal.length - 1]) * btcVal[0])).toFixed(4);
-            if (btcHourDiff >= 0) {
-                document.getElementById('btcHourDiff').textContent = " +" + btcHourDiff;
-            } else {
-                document.getElementById('btcHourDiff').textContent = btcHourDiff;
-            }
+      // Last hour diff
+      const btcHourDiff = (100 - ((100 / btcVal[btcVal.length - 1]) * btcVal[0])).toFixed(4);
+      if (btcHourDiff >= 0) {
+        document.getElementById('btcHourDiff').textContent = " +" + btcHourDiff;
+      } else {
+        document.getElementById('btcHourDiff').textContent = btcHourDiff;
+      }
 
-            // Chart
-            updateChart();
+      // Chart
+      updateChart();
 
-            
-            resetTimePassedInterval();
-            startTime = Date.now(); // Reset the start time
 
-            setTimeout(SiteRefresh, (refreshTime * 1000));
-        });
+      resetTimePassedInterval();
+      startTime = Date.now(); // Reset the start time
+
+      setTimeout(SiteRefresh, (refreshTime * 1000));
+    });
 })();
 
 
@@ -54,45 +53,64 @@ function resetTimePassedInterval() {
 
 
 
-document.getElementById('since_close').textContent = btcVal[btcVal.length - 1] ;
+document.getElementById('since_close').textContent = btcVal[btcVal.length - 1];
 
 
 //Graph
-function updateChart(){
-const xValues = Array.from({ length: 60 }, (_, i) => 60 - i); // Creating an array from 60 to 1
-const name = ['Bitcoin', 'Bot balance'];
+function updateChart() {
+  const xValues = Array.from({ length: 60 }, (_, i) => 60 - i); // Creating an array from 60 to 1
+  const name = ['Bitcoin', 'Bot balance'];
 
-new Chart("myChart", {
-  type: "line",
-  data: {
-    labels: xValues,
-    datasets: [{
-      data: btcVal, 
-      borderColor: "#da9940",
-      fill: false,
-      label: name[0]
-    }, /*{
+  new Chart("myChart", {
+    type: "line",
+    data: {
+      labels: xValues,
+      datasets: [{
+        data: btcVal,
+        borderColor: "#da9940",
+        fill: false,
+        label: name[0]
+      }, /*{
       data: [1600, 1700, 1700, 1900, 2000, 2700, 4000, 5000, 6000, 7000],
       borderColor: "lime",
       fill: false,
       label: name[1]
     }*/]
-  },
-  options: {
-    legend: { display: false }
-  }
-});
+    },
+    options: {
+      legend: { display: false }
+    }
+  });
 }
 
-function startTrading()
-{
+function startTrading() {
+  fetch('/start_trading', {
+    method: 'POST',
+  }).then(response => {
+    if (response.ok) {
+      console.log('Trading started');
+    } else {
+      console.error('Failed to start trading');
+    }
+  });
+
   loadData();
   startCounting();
 }
 
 
-function stopTrading()
-{
+function stopTrading() {
+  fetch('/stop_trading', {
+    method: 'POST',
+  }).then(response => {
+    if (response.ok) {
+      console.log('Trading stopped');
+    } else {
+      console.error('Failed to stop trading');
+    }
+  });
+
+
   clearInterval(intervalId); // Stop the counting interval before saving
   saveData();
 }
@@ -100,91 +118,91 @@ function stopTrading()
 function loadData() {
   // Fetch the trading data from the server
   fetch('/load_trading_data', {
-      method: 'GET',
-      headers: {
-          'Content-Type': 'application/json'
-      }
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
   })
-  .then(response => {
+    .then(response => {
       if (!response.ok) {
-          throw new Error('Network response was not ok');
+        throw new Error('Network response was not ok');
       }
       return response.json();
-  })
-  .then(data => {
+    })
+    .then(data => {
       if (data.trading_data) {
-          //console.log(data.tradingData);
-          // Update the content of the spans with the trading data
-          document.getElementById('BTC_balance').textContent = data.trading_data['BTC_balance'];
-          document.getElementById('USD_balance').textContent = data.trading_data['USD_balance'];
-          
-          document.getElementById('timeSpentTrading').innerText = data.trading_data['time_spent_trading'];
-          
-          document.getElementById('money_made').innerText = (data.trading_data['total_profit'] - data.trading_data['total_loss']);
-          
-          document.getElementById('good_trades').textContent = data.trading_data['good_trade_count'];
-          document.getElementById('bad_trades').textContent = data.trading_data['bad_trade_count'];
+        //console.log(data.tradingData);
+        // Update the content of the spans with the trading data
+        document.getElementById('BTC_balance').textContent = data.trading_data['BTC_balance'];
+        document.getElementById('USD_balance').textContent = data.trading_data['USD_balance'];
 
-          document.getElementById('long_count').textContent = data.trading_data['long_count'];
-          document.getElementById('short_count').textContent = data.trading_data['short_count'];
+        document.getElementById('timeSpentTrading').innerText = data.trading_data['time_spent_trading'];
 
-          document.getElementById('leverage').textContent = data.trading_data['leverage'];
-          
+        document.getElementById('money_made').innerText = (data.trading_data['total_profit'] - data.trading_data['total_loss']);
+
+        document.getElementById('good_trades').textContent = data.trading_data['good_trade_count'];
+        document.getElementById('bad_trades').textContent = data.trading_data['bad_trade_count'];
+
+        document.getElementById('long_count').textContent = data.trading_data['long_count'];
+        document.getElementById('short_count').textContent = data.trading_data['short_count'];
+
+        document.getElementById('leverage').textContent = data.trading_data['leverage'];
+
       } else {
-          console.error('Error: Missing trading data in response:', data);
+        console.error('Error: Missing trading data in response:', data);
       }
-  })
-  .catch(error => {
+    })
+    .catch(error => {
       console.error('Error fetching or processing trading data:', error);
-  });
+    });
 }
 
 let intervalId; // Variable to store the interval ID
 let timeSpent = 0; // Variable to store the time spent trading
 
 function startCounting() {
-    intervalId = setInterval(() => {
-        timeSpent++; // Increase the time spent trading by 1 second
-        document.getElementById('timeSpentTrading').innerText = formatTime(timeSpent);
-    }, 1000);
+  intervalId = setInterval(() => {
+    timeSpent++; // Increase the time spent trading by 1 second
+    document.getElementById('timeSpentTrading').innerText = formatTime(timeSpent);
+  }, 1000);
 }
 
 // Format time in HH:MM:SS format
 function formatTime(seconds) {
-    var hours = Math.floor(seconds / 3600);
-    var minutes = Math.floor((seconds % 3600) / 60);
-    var remainingSeconds = seconds % 60;
-    return pad(hours) + ":" + pad(minutes) + ":" + pad(remainingSeconds);
+  var hours = Math.floor(seconds / 3600);
+  var minutes = Math.floor((seconds % 3600) / 60);
+  var remainingSeconds = seconds % 60;
+  return pad(hours) + ":" + pad(minutes) + ":" + pad(remainingSeconds);
 }
 
 // Add leading zero if needed
 function pad(value) {
-    return value < 10 ? "0" + value : value;
+  return value < 10 ? "0" + value : value;
 }
 
 
 
 function saveData() {
   fetch('/load_trading_data')
-  .then(response => response.json())
-  .then(data => {
+    .then(response => response.json())
+    .then(data => {
       var tradingData = {};
-      tradingData['time_spent_trading'] = formatTime(timeSpent); 
-      
+      tradingData['time_spent_trading'] = formatTime(timeSpent);
+
       // Update the specific key-value pair you want to update in the trading data
       // For example, let's update the 'time_spent_trading' key
       fetch('/update_trading_data', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ key: 'time_spent_trading', value: tradingData['time_spent_trading'] })
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ key: 'time_spent_trading', value: tradingData['time_spent_trading'] })
       })
-      .then(response => response.json())
-      .then(data => console.log(data.message))
-      .catch(error => console.error('Error:', error));
-  })
-  .catch(error => console.error('Error:', error));
+        .then(response => response.json())
+        .then(data => console.log(data.message))
+        .catch(error => console.error('Error:', error));
+    })
+    .catch(error => console.error('Error:', error));
 }
 
 function resetSavedData() {
@@ -194,9 +212,9 @@ function resetSavedData() {
       'Content-Type': 'application/json'
     }
   })
-  .then(response => response.json())
-  .then(data => console.log(data.message))
-  .catch(error => console.error('Error:', error));
+    .then(response => response.json())
+    .then(data => console.log(data.message))
+    .catch(error => console.error('Error:', error));
   timeSpent = 0
 }
 
@@ -211,51 +229,51 @@ function sliderUpdate(value) {
 function switchLanguage() {
   var currentLang = document.documentElement.lang;
   var targetLang = currentLang === "en" ? "cz" : "en";
-  
+
   fetch('/static/translations.json')
-      .then(response => response.json())
-      .then(data => {
-        // Get all elements with data-translation attribute
-        var elements = document.querySelectorAll('[data-translation]');
+    .then(response => response.json())
+    .then(data => {
+      // Get all elements with data-translation attribute
+      var elements = document.querySelectorAll('[data-translation]');
 
-        // Update content of each element with the corresponding translation
-        elements.forEach(element => {
-          var translationKey = element.getAttribute('data-translation');
-          element.textContent = data[targetLang][translationKey];
-        });
+      // Update content of each element with the corresponding translation
+      elements.forEach(element => {
+        var translationKey = element.getAttribute('data-translation');
+        element.textContent = data[targetLang][translationKey];
+      });
 
-        // Set the document language to the target language
-        document.documentElement.lang = targetLang;
-        console.log(targetLang);
-      })
-      .catch(error => console.error('Error fetching translations:', error));
+      // Set the document language to the target language
+      document.documentElement.lang = targetLang;
+      console.log(targetLang);
+    })
+    .catch(error => console.error('Error fetching translations:', error));
 }
 
 
 
 
 function darkMode() {
-  document.documentElement.style.setProperty('--darkBlue', '#010207' )
+  document.documentElement.style.setProperty('--darkBlue', '#010207')
   document.documentElement.style.setProperty('--darkerBlue', '#000000')
   document.documentElement.style.setProperty('--yellow', '#fce6bd',)
   document.documentElement.style.setProperty('--orange', '#fcdb9f',)
-  document.documentElement.style.setProperty('--darkCyan','#04090f',) 
+  document.documentElement.style.setProperty('--darkCyan', '#04090f',)
 }
 
 function lightMode() {
-  document.documentElement.style.setProperty('--darkBlue', '#fcfcfc', )
+  document.documentElement.style.setProperty('--darkBlue', '#fcfcfc',)
   document.documentElement.style.setProperty('--darkerBlue', '#f7f7f7',)
   document.documentElement.style.setProperty('--yellow', '#000000',)
   document.documentElement.style.setProperty('--orange', '#000000',)
-  document.documentElement.style.setProperty('--darkCyan','#d3d3d3',)
+  document.documentElement.style.setProperty('--darkCyan', '#d3d3d3',)
 }
 
 function defaultMode() {
-  document.documentElement.style.setProperty('--darkBlue', '#041c32', )
+  document.documentElement.style.setProperty('--darkBlue', '#041c32',)
   document.documentElement.style.setProperty('--darkerBlue', '#021a31',)
   document.documentElement.style.setProperty('--yellow', '#ecb365',)
   document.documentElement.style.setProperty('--orange', '#da9940',)
-  document.documentElement.style.setProperty('--darkCyan','#04293a',)
+  document.documentElement.style.setProperty('--darkCyan', '#04293a',)
 }
 
 
