86cb9f84e27a59246691fed75e7ddcce657f8478
Adam4936 <adam.travnicek@student.gyarab.cz>
2024-04-21 15:01:00 +0200

    blobcolorchange

--- static/logic.js
+++ static/logic.js
@@ -320,7 +320,7 @@ function darkMode() {
   document.documentElement.style.setProperty('--yellow', '#fce6bd',)
   document.documentElement.style.setProperty('--orange', '#fcdb9f',)
   document.documentElement.style.setProperty('--darkCyan','#04090f',)
-  document.documentElement.style.setProperty('--blobColor', '#e8be87')
+  document.documentElement.style.setProperty('--blobColor', '#b8915e')
   document.getElementsByClassName("icon")[0].src = "static/img/Logo.png";
   updateChart();
   
